1c1ae7fcf7211796ac312480216de2c2fbb5c4d5
Dmytro Rubizhenko <d.rubizhenko@gmail.com>
2020-02-10 14:54:45 +0200

    add static html

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
+// Boolean type
 let isDone: boolean = false;
 
-console.log(isDone);
+// Number type
+let decimal: number = 12;
+
+// String type
+let str: string = "Hello";
+
+let res: string = `${str} world`;
+
+// Arrays
+const variant1: number[] = [12, 23, 69];
+const variant2: Array<number> = [12, 23, 69];
+
+// Tuple
+let coord: [number, number];
+
+coord = [32, 12];
+// Enum
+
+enum Color {
+  Red = "#ff0000",
+  Green = "#00ff00",
+  Blue = "#0000ff"
+}
+
+let c: Color = Color.Red;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
@@ -19,5 +20,14 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: "localhost",
+      port: 4242,
+      server: { baseDir: ["dist"] }
+    })
+  ]
 };
